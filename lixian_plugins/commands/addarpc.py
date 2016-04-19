6e3b3ddb661d801dbb8494ca10575bb2d556d831
slothg <slothg@163.com>
2016-04-19 22:46:46 +0800

    合并add download-aria2rpc功能至addarpc

--- lixian_plugins/commands/addarpc.py
+++ lixian_plugins/commands/addarpc.py
@@ -1,21 +1,49 @@
 
 from lixian_plugins.api import command
-
-from lixian_config import *
 from lixian_encoding import default_encoding
-from lixian_cli_parser import command_line_parser
-from lixian_cli_parser import with_parser
-from lixian_cli_parser import command_line_option, command_line_value
-from lixian_commands.util import parse_login, create_client
+
+from lixian_commands.util import *
+from lixian_cli_parser import *
+from lixian_config import get_config
+import lixian_help
+import lixian_query
 
 import urllib2
 import json
 
 
-def get_download_task_info(args, client):
+DEFAULT_JSONRPC = 'http://127.0.0.1:6800/jsonrpc'
 
-    import lixian_query
-    tasks = lixian_query.search_tasks(client, args)
+
+@command(usage = r'add tasks to Xunlei cloud and download with aria2rpc')
+@command_line_parser()
+@with_parser(parse_login)
+@with_parser(parse_colors)
+@with_parser(parse_logging)
+@with_parser(parse_size)
+@command_line_value('limit', default=get_config('limit'))
+@command_line_value('page-size', default=get_config('page-size'))
+@command_line_value('input', alias='i')
+@command_line_option('torrent', alias='bt')
+def addarpc(args):
+    '''
+    usage:
+        python lixian_cli.py add url
+        python lixian_cli.py add 1.torrent
+        python lixian_cli.py add torrent-info-hash
+        python lixian_cli.py add --bt http://xxx/xxx.torrent
+    '''
+    assert len(args) or args.input
+    client = create_client(args)
+    tasks = lixian_query.find_tasks_to_download(client, args)
+    print 'All tasks added. Checking status...'
+    columns = ['id', 'status', 'name']
+    if get_config('n'):
+        columns.insert(0, 'n')
+    if args.size:
+        columns.append('size')
+    output_tasks(tasks, columns, args)
+
     files = []
     for task in tasks:
         if task['type'] == 'bt':
@@ -29,28 +57,8 @@ def get_download_task_info(args, client):
                     files.append((f['xunlei_url'], f['name'], task['name']))
         else:
             files.append((task['xunlei_url'], task['name'], None))
-    return files
 
-
-def export_download_task_info(files, client):
-    output = ''
     for url, name, dir in files:
-        if type(url) == unicode:
-            url = url.encode(default_encoding)
-        output += url + '\n'
-        output += '  out=' + name.encode(default_encoding) + '\n'
-        if dir:
-            output += '  dir=' + dir.encode(default_encoding) + '\n'
-        output += '  header=Cookie: gdriveid=' + client.get_gdriveid() + '\n'
-    return output
-
-
-def execute_download_aria2rpc(args):
-    client = create_client(args)
-    task_list = get_download_task_info(args, client)
-    # print(export_download_task_info(task_list, client))
-
-    for url, name, dir in task_list:
         if type(url) == unicode:
             url = url.encode(default_encoding)
         if dir:
@@ -67,23 +75,14 @@ def execute_download_aria2rpc(args):
                                          }
                               ]
                               })
-        # print(jsonreq)
-        c = urllib2.urlopen("http://127.0.0.1:6800/jsonrpc", jsonreq)
-        # {u'jsonrpc': u'2.0', u'id': u'qwer', u'result': u'f1257fa333f235e6'}
+        jsonrpc = get_config('aria2jsonrpc')
+        if jsonrpc is None:
+            global DEFAULT_JSONRPC
+            jsonrpc = DEFAULT_JSONRPC
+        c = urllib2.urlopen(jsonrpc, jsonreq)
         result = c.read()
         if result is None or result == "":
             print("\033[31mCann't add aria2 task %s\033[0m" % name)
         else:
             result = json.loads(result.decode(default_encoding))
             print("\033[32mAdd aria2 task[id= %s] %s\033[0m" % (result[u"result"], name))
-
-
-@command(usage='concurrently download tasks in aria2 with rpc')
-@command_line_parser()
-@with_parser(parse_login)
-@command_line_option('all')
-def download_aria2rpc(args):
-    '''
-    usage: lx download-aria2rpc -j 5 [id|name]...
-    '''
-    execute_download_aria2rpc(args)
